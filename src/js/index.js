94b9735c9a8e8f102daf6b28ff72e98bdf68496f
Michelle Oliveira <66038822+michelleoliveirah@users.noreply.github.com>
2023-03-13 13:34:36 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,4 @@
-// Objetivo 1 
-// passo 1- pegar o elemento que representa o botão na tela usando o Js;
-// passo 2- identificar quando o usuário clicar no botão;
-// passo 3- pegar o elemento da modal no Js;
-// passo 4 - abrir modal na tela;
 
-//Objetivo 2 - Quando o usuário clicar no "x" fechar a modal;
-
-// passo 1- pegar o elemento de fechar modal;
-
-// passo 2- identificar quando o usuário clicar no "x";
-//passo 3- fechar a modal;
 
 const botaoTrailer = document.querySelector(".botao-trailer");
 const botaofecharModal = document.querySelector(".fechar-modal");
